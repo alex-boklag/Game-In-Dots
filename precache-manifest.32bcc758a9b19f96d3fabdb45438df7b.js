2069c1bf791d6c10f0c5a2c750956842c8e105f2
alexboklag <sanya32102@gmail.com>
2020-04-23 21:07:58 +0300

    Updates

---
+++ precache-manifest.32bcc758a9b19f96d3fabdb45438df7b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "947bad83fb10614ac5622986143678b0",
+    "url": "/game-in-dots/index.html"
+  },
+  {
+    "revision": "b51eaa4fa973da28d411",
+    "url": "/game-in-dots/static/css/main.498a60fa.chunk.css"
+  },
+  {
+    "revision": "bb8b68d4ae820347cb1f",
+    "url": "/game-in-dots/static/js/2.e341e55c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/game-in-dots/static/js/2.e341e55c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b51eaa4fa973da28d411",
+    "url": "/game-in-dots/static/js/main.065a55e2.chunk.js"
+  },
+  {
+    "revision": "52e6d185a13275c72550",
+    "url": "/game-in-dots/static/js/runtime-main.6d5097c9.js"
+  }
+]);
